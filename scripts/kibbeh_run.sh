55f2e6eee2d40c4d5d59d46d3fa32242f6564b37
pr <pineapplerind.info@gmail.com>
2023-05-24 17:39:46 -0400

    add skip install flag to kibbeh_run&ignore output/

--- scripts/kibbeh_run.sh
+++ scripts/kibbeh_run.sh
@@ -1,1 +1,12 @@
-cd packages/kibbeh && pip install -r requirements.txt && python main
+cd packages/kibbeh 
+
+
+if [[ $1 != "-"*"i"* ]]
+then
+    echo Installing requirements...
+    pip install -r requirements.txt
+else
+    echo Skipped installing requirements
+fi
+
+sanic main
